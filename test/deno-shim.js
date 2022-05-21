4e895553552ecf8d78d1b9eecfb3397a8ece950f
Daniel Lamando <dan@danopia.net>
2022-05-21 11:59:41 +0200

    Update std to /std@0.140.0

--- test/deno-shim.js
+++ test/deno-shim.js
@@ -1,7 +1,7 @@
 import {
   assertEquals, assertStrictEquals,
   assertThrows, assertStringIncludes,
-} from "https://deno.land/std@0.105.0/testing/asserts.ts";
+} from "https://deno.land/std@0.140.0/testing/asserts.ts";
 
 const suiteStack = [];
 
